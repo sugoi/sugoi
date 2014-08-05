3ac191d8cc153315e85930b02434976fb61ea418
Takayuki Muranushi <muranushi@google.com>
2014-08-05 16:03:32 +0900

    write a main loop.

--- Sugoi/Run.hs
+++ Sugoi/Run.hs
@@ -1,13 +1,50 @@
+{-# LANGUAGE ScopedTypeVariables, TypeFamilies #-}
 module Sugoi.Run where
 
+import Control.Applicative ((<|>))
 import qualified Data.Text as T
+import qualified Data.Vector as V
+import Data.Vector ((!))
+
+import Control.Lens
+import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.State
+import System.Random
 
 import Sugoi.Types
 
-mainLoop :: (Monad m, MonadIO m, MonadState s m, HasFarmState s m a) 
-  => Farm m a -> m ()
-mainLoop farm = do
+mainLoop :: forall env m s. (m ~ MonadOf env, Monad m, MonadIO m, MonadState s m, HasFarm s env) 
+  => m ()
+mainLoop = do
+  bank0 <- use genomBank
+  breeder0 <- use breeder
+  let 
+    deck0 :: Deck env
+    deck0 (weightFunction, nD) = do
+      let wbank :: V.Vector (ResumeOf env, Double)
+          wbank = V.map (\r -> (r, weightFunction r)) bank0
+
+          nB :: Int
+          nB = V.length bank0
+
+          pileOfW :: V.Vector Double
+          pileOfW = V.generate nB $ \i ->
+            (snd $ wbank!i) + (if i == 0 then 0 else pileOfW ! (i-1))
+
+          sumOfW :: Double
+          sumOfW = V.last pileOfW 
+            
+
+      replicateM nD $ do
+        realIdx <- liftIO $ randomRIO (0, sumOfW)
+        let i = maybe (nB-1) id $ V.findIndex (> realIdx) pileOfW 
+        return $ bank0!i
+
+  newGenome <- breeder0 deck0
+  msmt <- use measurement
+  bench <- msmt newGenome  
+  genomBank %= (flip V.snoc (newGenome, [bench]))
+  
   return ()
   
--- Sugoi/Types.hs
+++ Sugoi/Types.hs
@@ -6,26 +6,29 @@ import qualified Data.Text as T
 import qualified Data.Vector as V
 
 
-type family GenomOf :: * -> *
-type family MonadOf :: * -> (* -> *)
-type family ScoreOf :: * -> *     
+class FarmEnvironment env where
+  type GenomOf env :: *
+  type MonadOf env :: * -> *
+  type BenchmarkOf env :: *
 
 
-type Draw a = ((a -> Double) , Int)
-type Deck m a = Draw a -> m [a]
-type Breeder m a = Deck m a -> m a  
+type ResumeOf env = (GenomOf env, [BenchmarkOf env])
 
-data Farm m a = Farm
-  { _breeder :: Breeder m a
-  , _encoder :: a -> T.Text
-  , _decoders :: [T.Text -> Maybe a]   
+
+
+
+type Draw e = ((ResumeOf e -> Double) , Int) 
+type Deck e = Draw e -> (MonadOf e) [ResumeOf e]
+type Breeder e = Deck e -> (MonadOf e) (GenomOf e)
+
+data Farm e = Farm
+  { _breeder :: Breeder e
+  , _encoder :: ResumeOf e -> T.Text
+  , _decoders :: [T.Text -> Maybe (ResumeOf e)]   
+  , _measurement :: GenomOf e -> (MonadOf e) (BenchmarkOf e)
+  , _genomBank :: V.Vector (ResumeOf e)
   }
-  
+
 makeClassy ''Farm
 
-data FarmState m a = FarmState
-  { _database :: V.Vector a
-  , _encodeDatabse :: V.Vector a -> m ()
-  }
-
-makeClassy ''FarmState
+  
